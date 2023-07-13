0899da73c0397843c2e0db5a04ec7c65738f6c04
Vickie1394 <107905614+Vickie1394@users.noreply.github.com>
2023-07-13 11:02:45 -0500

    Add files via upload

---
+++ dbo.Appointment.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [PatientName] NVARCHAR(50) NOT NULL, 
+    [Reason] NVARCHAR(MAX) NOT NULL, 
+    [DateAppointment] DATE NOT NULL
+)
