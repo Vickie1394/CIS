e0ea3f2ff647c3762875ebd0b49424f742cf9daa
Vickie1394 <107905614+Vickie1394@users.noreply.github.com>
2023-07-13 11:03:20 -0500

    Add files via upload

---
+++ dbo.Patient.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Patient] (
+    [PatientId]        UNIQUEIDENTIFIER NOT NULL,
+    [Name]             NVARCHAR (50)    NULL,
+    [PhoneNo]          NVARCHAR (50)    NULL,
+    [TreatmentDetails] NVARCHAR (50)    NULL,
+    [Gender]              NVARCHAR (50)    NULL,
+    [Email]            NVARCHAR (50)    NOT NULL,
+    [BloodGrouup]      NCHAR (10)       NULL,
+    CONSTRAINT [PK_Patient] PRIMARY KEY CLUSTERED ([PatientId] ASC)
+);
+
